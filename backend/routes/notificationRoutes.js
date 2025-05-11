de966ed2993622ad6fb38aee68d9f14120faf539
Ansh Khurana <anshkhurana042003@gmail.com>
2025-05-12 00:32:31 +0530

    Restore repo structure and removed unnecessary files

--- backend/routes/notificationRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getUserNotifications } from "../controllers/notificationController.js";
-import { auth } from "../middleware/authMiddleware.js";
-
-const router = express.Router();
-
-router.get("/", auth, getUserNotifications);
-
-export default router;
